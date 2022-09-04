1e0482e3271516323167e8636b42ceb598ef1706
Tushar-Gautam <tushargautam999@gmail.com>
2022-09-05 02:15:12 +0530

    first Commit

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -5,11 +5,68 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number < 0) {
+      return people.length - 1;
+    }
+    if (number > people.length - 1) {
+      return 0;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      // if (newIndex === people.length) {
+      //   return (newIndex = 0);
+      // }
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      // if (newIndex < 0) {
+      //   return people.length - 1;
+      // }
+      return checkNumber(newIndex);
+    });
+  };
+
+  const surprise = () => {
+    setIndex((index) => {
+      let newIndex = Math.floor(Math.random() * people.length);
+      // console.log(newIndex);
+      if (newIndex === index) {
+        newIndex = index + 1;
+      }
+      return checkNumber(newIndex);
+    });
+  };
   return (
     <article className="review">
       <div className="img-container">
         <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
       </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={surprise}>
+        Surprise me!!
+      </button>
     </article>
   );
 };
