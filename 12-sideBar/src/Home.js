bafac2a031c0b0ebfef82f6fe6f1bd283a6ba97d
Tushar-Gautam <tushargautam999@gmail.com>
2022-09-14 17:47:47 +0530

    first

---
+++ 12-sideBar/src/Home.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./context";
+
+const Home = () => {
+  const { openSideBar, openModal } = useGlobalContext();
+  // console.log(data);
+
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSideBar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        Show Modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
