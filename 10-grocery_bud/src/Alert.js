7574bf73cf03ead15a5794effbc984952c4d555f
Tushar-Gautam <tushargautam999@gmail.com>
2022-11-06 03:07:36 +0530

    empty value bug removed

--- 10-grocery_bud/src/Alert.js
+++ 10-grocery_bud/src/Alert.js
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const Alert = ({ type, msg, removeAlert, list }) => {
   useEffect(() => {
-    const timeout = setTimeout(() => {
+    const timeOut = setTimeout(() => {
       removeAlert();
     }, 3000);
-    return () => clearTimeout(timeout);
+    return () => clearTimeout(timeOut);
   }, [list]);
-  return <p className={`alert alert-${type}`}>{msg}</p>;
+  //without list in this array if we change the alert like from item added to delete it will work for 3 seconds in total and not 3 seconds after the list has been updates.
+
+  return (
+    <>
+      <p className={`alert alert-${type}`}>{msg}</p>
+    </>
+  );
 };
 
 export default Alert;
--- 10-grocery_bud/src/App.js
+++ 10-grocery_bud/src/App.js
@@ -1,95 +1,114 @@
-import React, { useState, useEffect } from 'react';
-import List from './List';
-import Alert from './Alert';
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
 const getLocalStorage = () => {
-  let list = localStorage.getItem('list');
+  let list = localStorage.getItem("list");
   if (list) {
-    return (list = JSON.parse(localStorage.getItem('list')));
+    return JSON.parse(localStorage.getItem("list"));
   } else {
     return [];
   }
 };
+
 function App() {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
-  const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    // console.log(e);
     if (!name) {
-      showAlert(true, 'danger', 'please enter value');
+      //display Alert
+      showAlert(true, "danger", "please enter some value");
     } else if (name && isEditing) {
+      //deal with edit
       setList(
         list.map((item) => {
-          if (item.id === editID) {
+          if (item.id === editId) {
             return { ...item, title: name };
           }
           return item;
         })
       );
-      setName('');
-      setEditID(null);
+      setName("");
+      setEditId("");
       setIsEditing(false);
-      showAlert(true, 'success', 'value changed');
+      showAlert(true, "success", "Value Changed");
     } else {
-      showAlert(true, 'success', 'item added to the list');
-      const newItem = { id: new Date().getTime().toString(), title: name };
-
+      showAlert(true, "success", "item added");
+      let newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
-      setName('');
+      setName("");
     }
   };
 
-  const showAlert = (show = false, type = '', msg = '') => {
-    setAlert({ show, type, msg });
+  const showAlert = (show = false, type = "", msg = "") => {
+    return setAlert({ show, type, msg });
   };
+
   const clearList = () => {
-    showAlert(true, 'danger', 'empty list');
+    showAlert(true, "danger", "All items removed");
     setList([]);
   };
-  const removeItem = (id) => {
-    showAlert(true, 'danger', 'item removed');
+
+  const removeItems = (id) => {
+    showAlert(true, "danger", "item removed");
     setList(list.filter((item) => item.id !== id));
   };
+
   const editItem = (id) => {
     const specificItem = list.find((item) => item.id === id);
     setIsEditing(true);
-    setEditID(id);
+    setEditId(id);
     setName(specificItem.title);
   };
-  useEffect(() => {
-    localStorage.setItem('list', JSON.stringify(list));
-  }, [list]);
-  return (
-    <section className='section-center'>
-      <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
 
-        <h3>grocery bud</h3>
-        <div className='form-control'>
-          <input
-            type='text'
-            className='grocery'
-            placeholder='e.g. eggs'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button type='submit' className='submit-btn'>
-            {isEditing ? 'edit' : 'submit'}
-          </button>
-        </div>
-      </form>
-      {list.length > 0 && (
-        <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} editItem={editItem} />
-          <button className='clear-btn' onClick={clearList}>
-            clear items
-          </button>
-        </div>
-      )}
-    </section>
+  //below to store data in local memory whenver list changes
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <>
+      <section className="section-center">
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && (
+            <Alert {...alert} removeAlert={showAlert} list={list} />
+          )}
+          <label htmlFor="things">
+            <h3>grocery bud</h3>
+
+            <div className="form-control">
+              <input
+                type="text"
+                className="grocery"
+                value={name}
+                id="things"
+                placeholder="e.g.eggs"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <button className="submit-btn">
+                {isEditing ? "Editing" : "Submit"}
+              </button>
+            </div>
+          </label>
+        </form>
+        {list.length > 0 && (
+          <div className="grocery-container">
+            <List items={list} removeItem={removeItems} editItem={editItem} />
+            <button className="clear-btn" onClick={() => clearList()}>
+              Clear All
+            </button>
+          </div>
+        )}
+      </section>
+    </>
   );
 }
-
 export default App;
