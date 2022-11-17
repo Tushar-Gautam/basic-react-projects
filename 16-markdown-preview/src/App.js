9a47244ea455e4f4a0fbed0b5411e67dca9751e4
Tushar-Gautam <tushargautam999@gmail.com>
2022-11-17 19:59:19 +0530

    final

--- 16-markdown-preview/src/App.js
+++ 16-markdown-preview/src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
-
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import data from "./data";
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [markdown, setMarkdown] = useState(data);
+
+  return (
+    <>
+      <div>
+        <section className="markdown">
+          <textarea
+            className="input"
+            value={markdown}
+            onChange={(e) => setMarkdown(e.target.value)}
+          ></textarea>
+          <article className="result">
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+          </article>
+        </section>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
